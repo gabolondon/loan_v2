58d3488969c9914044e33a9f23b09121c0382897
Gabriel Londono <gabriel@circuloten.com>
2025-01-11 10:44:49 -0500

    added users to dashboard home

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
     import { LogOut, Home, Users, Wallet } from 'lucide-react';
-    import { useStore } from '../store/useStore';
-    import { UserManagement } from './UserManagement';
+    import { useStore } from "../store/useStore";
     import { auth } from '../config/firebase';
     import { NavLink, Outlet, useLocation } from 'react-router-dom';
     import { Breadcrumb } from './Breadcrumb';
@@ -34,8 +33,8 @@ import React from 'react';
       ];
 
       return (
-        <div className="min-h-screen bg-gray-50 flex">
-          <aside className="w-64 bg-gray-100 border-r p-4 flex flex-col">
+        <div className="flex h-screen bg-gray-50">
+          <aside className="flex flex-col w-64 p-4 bg-gray-100 border-r">
             <div className="mb-8">
               <h1 className="text-2xl font-bold text-gray-900">Loan Manager</h1>
             </div>
@@ -48,7 +47,7 @@ import React from 'react';
                     to={item.path}
                     className={({ isActive }) =>
                       `flex items-center px-4 py-2 rounded-md hover:bg-gray-200 ${
-                        isActive ? 'bg-gray-200 font-semibold' : 'text-gray-700'
+                        isActive ? "bg-gray-200 font-semibold" : "text-gray-700"
                       }`
                     }
                   >
@@ -58,13 +57,13 @@ import React from 'react';
                 );
               })}
             </nav>
-            <div className="mt-auto pt-4 border-t">
+            <div className="pt-4 mt-auto border-t">
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
                   <img
                     src={user?.photoURL}
                     alt={user?.name}
-                    className="w-8 h-8 rounded-full mr-2"
+                    className="w-8 h-8 mr-2 rounded-full"
                   />
                   <div>
                     <p className="text-sm font-semibold text-gray-800">
@@ -82,7 +81,7 @@ import React from 'react';
               </div>
             </div>
           </aside>
-          <main className="flex-1 p-4">
+          <main className="flex-1 h-screen px-6 py-4 overflow-y-auto">
             <Breadcrumb location={location} />
             <Outlet />
           </main>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { LoanList } from "./LoanList";
+import { useStore } from "../store/useStore";
+import { UserManagement } from "./UserManagement";
 
 export const Home: React.FC = () => {
+  const { user } = useStore();
   return (
     <div>
       <h2 className="mb-4 text-2xl font-semibold">
         Welcome to the Loan Manager
       </h2>
       <LoanList />
+      {user?.isAdmin && <UserManagement />}
     </div>
   );
 };
